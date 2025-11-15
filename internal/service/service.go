12995e3379a0a88c2dbbcdf822a5949c99341f27
DenHax <whatxpw@gmail.com>
2025-11-16 00:48:39 +0300

    feat(service): define `user`, `team`, `pullRequest` interfaces to implement in service bootstrap

--- internal/service/service.go
+++ internal/service/service.go
@@ -1,20 +1,42 @@
 package service
 
 import (
+	"github.com/DenHax/pr-reviews-avito/internal/domain/models"
 	"github.com/DenHax/pr-reviews-avito/internal/repo"
+	"github.com/DenHax/pr-reviews-avito/internal/service/pr"
+	"github.com/DenHax/pr-reviews-avito/internal/service/teams"
 	"github.com/DenHax/pr-reviews-avito/internal/service/users"
 )
 
 type Users interface {
-	GetAll()
+	SetUserActive(req models.SetActiveRequest) (*models.User, error)
+	GetUserReviews(userID string) (*models.ReviewListResponse, error)
+}
+
+type Teams interface {
+	CreateTeam(req models.TeamRequest) error
+	GetTeam(teamName string) (*models.TeamRequest, error)
+}
+
+type PullRequests interface {
+	CreatePR(req models.CreatePRRequest) (*models.PullRequest, error)
+	MergePR(pullRequestID string) (*models.PullRequest, error)
+	ReassignReviewer(req models.ReassignRequest) (*models.PullRequest, string, error)
 }
 
 type Service struct {
 	Users
+	Teams
+	PullRequests
 }
 
 func NewService(repos *repo.Repository) *Service {
+	userService := users.NewUserService(repos.Users, repos.PullRequests)
+	teamService := teams.NewTeamService(repos.Teams, *userService)
+	prService := pr.NewPRService(repos.PullRequests, *userService)
 	return &Service{
-		Users: users.NewUserService(repos.Users),
+		Users:        userService,
+		Teams:        teamService,
+		PullRequests: prService,
 	}
 }
